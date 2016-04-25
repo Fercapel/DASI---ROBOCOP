026f1ce66eac6d8aacb72decc0f5033bd3412115
gorco <antcal01@ucm.es>
2016-04-25 21:23:18 +0200

    Añadidos iconos y probando visualización

--- src/icaro/aplicaciones/recursos/recursoVisualizadorEntornosSimulacion/imp/robocop/VisorMovimientoRobocop.java
+++ src/icaro/aplicaciones/recursos/recursoVisualizadorEntornosSimulacion/imp/robocop/VisorMovimientoRobocop.java
@@ -3,6 +3,12 @@ package icaro.aplicaciones.recursos.recursoVisualizadorEntornosSimulacion.imp.ro
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class VisorMovimientoRobocop extends JFrame {
@@ -20,14 +26,26 @@ public class VisorMovimientoRobocop extends JFrame {
 	 * Inicializar contenido de la ventana.
 	 */
 	private void initialize() {
-		this.setBounds(100, 100, 450, 300);
+		this.setBounds(500, 500, 384, 384);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JFrame.setDefaultLookAndFeelDecorated(true);
-		this.getContentPane().setLayout(new GridLayout(3, 2));
+		this.getContentPane().setLayout(new GridLayout(8, 8));
 	    
-	    for(int i = 0; i < 6; i++){
-	    	this.getContentPane().add(new JButton("Button "+i));
+	    for(int i = 0; i < 64; i++){
+	    	//----
+	    	//Ejemplo (probando mapa)
+	    	//TODO leer mapa de archivo
+	    	String file = null;
+	    	if(i==0){
+	    		file = "comisaria";
+	    	} else if(i<8 || i%4==0 || (i>8*2&&i<8*3) || (i>8*4&&i<8*5) || (i>8*6&&i<8*7) ){
+	    		file = "calle";
+	    	} else {
+	    		file = "casa1";
+	    	}
+	    	//-----
+	    	this.getContentPane().add(getImage(file));
 	    }
 	    
 	    this.pack();
@@ -52,6 +70,18 @@ public class VisorMovimientoRobocop extends JFrame {
 				}
 			}
 		});
+	}
+	
+	private JButton getImage(String file){
+		JButton button = new JButton();
+		try {
+			Image img = ImageIO.read(getClass().getResource("iconos/"+file+".jpg"));
+			button.setIcon(new ImageIcon(img));
+			button.setBorder(null);
+		} catch (IOException ex) {
+		}
+  
+		return button;
 	}
 
 }
