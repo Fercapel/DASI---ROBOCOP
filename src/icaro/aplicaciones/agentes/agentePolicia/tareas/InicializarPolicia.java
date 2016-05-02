92f562b387a2a2a4c9780af0b5c4c1376d373348
gorco <antcal01@ucm.es>
2016-05-02 21:05:47 +0200

    Test de colocar el agente en el mapa

--- src/icaro/aplicaciones/agentes/agentePolicia/tareas/InicializarPolicia.java
+++ src/icaro/aplicaciones/agentes/agentePolicia/tareas/InicializarPolicia.java
@@ -1,5 +1,6 @@
 package icaro.aplicaciones.agentes.agentePolicia.tareas;
 
+import icaro.aplicaciones.recursos.recursoVisualizadorEntornosSimulacion.ItfUsoRecursoVisualizadorEntornoSimulacion;
 import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Focus;
 import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
 
@@ -8,14 +9,23 @@ public class InicializarPolicia extends TareaSincrona {
 	@Override
 	public void ejecutar(Object... params) {
 		try {
+			System.out.println(params[0]+"----"+params[1]);
 			// Objetivo objetivoEjecutantedeTarea = (Objetivo)params[0];
 			// String identTarea = this.getIdentTarea();
-			// String nombreAgenteEmisor = this.getIdentAgente();
+			
 			this.getItfConfigMotorDeReglas().setDepuracionActivationRulesDebugging(false);
 			this.getItfConfigMotorDeReglas().setfactHandlesMonitoring_afterActivationFired_DEBUGGING(false);
 			this.getEnvioHechos().insertarHechoWithoutFireRules(new Focus());
 			//this.getEnvioHechos().insertarHechoWithoutFireRules(new MisObjetivos());
 			//this.getEnvioHechos().insertarHechoWithoutFireRules(new VictimsToRescue());
+			
+			//Obtener recurso visualización
+			ItfUsoRecursoVisualizadorEntornoSimulacion itfCompMov = (ItfUsoRecursoVisualizadorEntornoSimulacion) repoInterfaces.obtenerInterfazUso("RecursoVisualizadorEntornoSimulacionRobocop");
+			/*
+			 *  Prueba concepto -> Poner al policía en la posicion 0,0 del mapa
+			 */
+			String idAgente = this.getIdentAgente();
+			itfCompMov.mostrarPosicionRobot(idAgente, 0, 0);
 
 		} catch (Exception e) {
 			e.printStackTrace();
